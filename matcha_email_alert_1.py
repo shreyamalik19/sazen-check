3e0e977edef881838e69e11f5cd31a4d2a5b6d79
shreyamalik19 <malikshreya95@gmail.com>
2025-05-20 15:42:05 -0700

    Update matcha_email_alert_1.py
    parse beautifulsoup str

--- matcha_email_alert_1.py
+++ matcha_email_alert_1.py
@@ -19,10 +19,10 @@ def is_matcha_available():
         soup = BeautifulSoup(response.text, 'html.parser')
         #print(soup.text)
         content = soup.find(id="content")
-        print ("content: " , content)
-        print("is matcha available? " , "Marukyu Koyamaen" in content)
-        print("is matcha available? " , "Kanbayashi Shunsho" in content)
-        return ("Marukyu Koyamaen" in content) or ("Yamamasa Koyamaen" in content) or ("Kanbayashi Shunsho" in content)
+        print ("content: " , str(content))
+        print("is matcha available? " , "Marukyu Koyamaen" in str(content))
+        print("is matcha available? " , "Kanbayashi Shunsho" in str(content))
+        return ("Marukyu Koyamaen" in str(content)) or ("Yamamasa Koyamaen" in str(content)) or ("Kanbayashi Shunsho" in str(content))
     except Exception as e:
         print("Error checking stock:", e)
         return False
